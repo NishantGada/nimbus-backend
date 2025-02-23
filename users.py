92bfd2c860b885a2cdf2004f6589d517b48812f0
NishantGada <nishant.gada160@gmail.com>
2025-02-23 16:40:15 -0500

    refactor: update user + get user details apis

--- users.py
+++ users.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, jsonify
 from dbconfig import get_connection
 import uuid
 
+from helper_functions import fetch_user_id_from_user_email
+
 users_bp = Blueprint('users', __name__)
 
 @users_bp.route('/users', methods=['POST'])
@@ -24,7 +26,6 @@ def create_user_logic():
         return jsonify({"error": str(e)}), 500
 
 def create_user(name, email, password):
-    """Creates a new user"""
     connection = get_connection()
     cursor = connection.cursor()
     
@@ -40,9 +41,8 @@ def create_user(name, email, password):
 
 @users_bp.route('/users', methods=['GET'])
 def get_all_users():
-    """Fetch all users"""
     connection = get_connection()
-    cursor = connection.cursor(dictionary=True)  # Use dictionary=True to return column names in results
+    cursor = connection.cursor(dictionary=True)
 
     cursor.execute("SELECT user_id, name, email FROM Users")
     users = cursor.fetchall()
@@ -51,3 +51,55 @@ def get_all_users():
     connection.close()
 
     return jsonify({"users": users})
+
+@users_bp.route('/users/details', methods=['POST'])
+def get_user_details():
+    data = request.json
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    user_id = fetch_user_id_from_user_email(data['user_email'])
+
+    cursor.execute("SELECT * FROM Users where user_id = %s", (user_id,))
+    user_details = cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    return jsonify({"user_details": user_details})
+
+@users_bp.route('/users', methods=['PUT'])
+def update_user_details():
+    data = request.json
+
+    # Remove the user_id from the update data (you don't want to update the ID)
+    fields_to_update = {key: value for key, value in data.items() if key not in ['user_id', 'created_at']}
+    print("fields_to_update: ", fields_to_update)
+    
+    if not fields_to_update:
+        return jsonify({"status": "error", "message": "No fields to update"}), 400
+    
+    # Generate the SET part of the query dynamically based on fields provided
+    set_clause = ', '.join([f"{key} = %s" for key in fields_to_update.keys()])
+    values = list(fields_to_update.values())
+
+    # Construct the SQL query
+    query = f"UPDATE users SET {set_clause} WHERE user_id = %s"
+    values.append(data["user_id"])
+
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+
+        # Execute the query
+        cursor.execute(query, values)
+        connection.commit()
+
+        return jsonify({"status": "success", "message": "User details updated successfully"}), 200
+
+    except Exception as e:
+        connection.rollback()
+        return jsonify({"status": "error", "message": str(e)}), 500
+    finally:
+        cursor.close()
+        connection.close()
